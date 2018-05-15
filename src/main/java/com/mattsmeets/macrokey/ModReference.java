1e9391e88bec83b18c6d96d7b37da636f072bd22
Mata <matthewsmeets@gmail.com>
2018-05-15 18:21:58 +0200

    Preparing 1.12-2.0 BETA build

--- src/main/java/com/mattsmeets/macrokey/ModReference.java
+++ src/main/java/com/mattsmeets/macrokey/ModReference.java
@@ -8,8 +8,7 @@ public class ModReference {
     public static final String MOD_ID = "macrokey";
     public static final String MOD_NAME = instance.referencePropLoader.getProperty("mod_name");
     public static final String MOD_VERSION = instance.referencePropLoader.getProperty("mod_version");
-
-    public static final String UPDATE_URL = "https://bitbucket.org/MattsMc/versionchecking/raw/master/macrokey.json";
+    public static final String UPDATE_URL = instance.referencePropLoader.getProperty("update_url");
 
     public static final String COMMON_PROXY = "com.mattsmeets.macrokey.proxy.CommonProxy";
     public static final String CLIENT_PROXY = "com.mattsmeets.macrokey.proxy.ClientProxy";
