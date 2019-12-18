861128271a725e43f96c1f4dc535d8a30f9a5e0a
Matthew Smeets <m.smeets@performation.com>
2019-12-18 17:43:20 +0100

    Enabling Chat module

--- src/main/java/com/mattsmeets/macrokey/api/ChatAPI.java
+++ src/main/java/com/mattsmeets/macrokey/api/ChatAPI.java
@@ -4,13 +4,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatAllowedCharacters;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
+import org.graalvm.polyglot.HostAccess;
 
 public class ChatAPI extends AbstractAPI {
 
+    @HostAccess.Export
     public void Send(String message) {
         Minecraft.getMinecraft().player.sendChatMessage(legalCharacters(message));
     }
 
+    @HostAccess.Export
     public void Add(String message) {
         Minecraft.getMinecraft().player.sendMessage(new TextComponentString(message));
     }
